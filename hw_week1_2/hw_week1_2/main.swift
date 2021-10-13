861e5598bb28c193435657f229e5f629f188bc11
Najla Talal <najlatalal@MacBook-Air-Najla.local>
2021-10-13 23:30:59 +0300

    Najla Talal hw2

---
+++ hw_week1_2/hw_week1_2/main.swift
@@ -0,0 +1,34 @@
+
+
+
+import Foundation
+print("Enter a visitor age : ", terminator: "")
+var x = Utils.readInt()
+var totalcoast: Double = 78.00
+
+    if totalcoast != 78.00 {
+
+    }
+    for (x) in 1...5 {
+          var = [String]() , = Utils.readInt()
+
+        if x <= 2 {
+            
+        }
+        if x <= 12 {
+    }
+        if x >= 65 {
+            
+        }
+        if <= 65 {
+            
+            
+        }
+
+while totalcoast != 0  {
+
+    print("Enter a visitor age")
+}
+    }
+
+
