df9671f50188044a8f2d32b02c8e8fc1c21adf5d
abdullah <abduulahalrashoudi@Abdullahs-MacBook-Pro.local>
2021-10-13 23:45:48 +0300

    Third Home Work Abdullah Mansoor

--- HW_01_04/HW_01_04/main.swift
+++ HW_01_04/HW_01_04/main.swift
@@ -7,30 +7,35 @@
 
 import Foundation
 
-print("Enter a visitor age (0 to exit): ", terminator: " ")
-var visitorsAge = Utils.readInt()
+
 
 var totalPrice: Double = 0
 
-repeat {
-    switch visitorsAge {
+while true {
+    print("Enter a visitor age (0 to exit): ", terminator: " ")
+    let visitorsAge = Utils.readInt()
+    if visitorsAge == 0 {
+        
+        break
+    }
+
+    else {  switch visitorsAge {
     case 1...2:
         totalPrice += 0
-        print("Enter a visitor age (0 to exit): ", terminator: " ")
-        visitorsAge = Utils.readInt()
-    case 3...12:
+        
+            case 3...12:
          totalPrice += 14
-        print("Enter a visitor age (0 to exit): ", terminator: " ")
-        visitorsAge = Utils.readInt()
+       
+     
     case 13...64:
         totalPrice += 23
-        print("Enter a visitor age (0 to exit): ", terminator: " ")
-        visitorsAge = Utils.readInt()
+        
     default:
         totalPrice += 18
-        print("Enter a visitor age (0 to exit): ", terminator: " ")
-        visitorsAge = Utils.readInt()
+        
     }
-} while visitorsAge != 0
+    }
+}
+var tax = totalPrice * 0.15
+print("The total cost: ", Float(totalPrice * 1.15), "the tax is \(tax)" )
 
-print("The total cost: ", totalPrice)
