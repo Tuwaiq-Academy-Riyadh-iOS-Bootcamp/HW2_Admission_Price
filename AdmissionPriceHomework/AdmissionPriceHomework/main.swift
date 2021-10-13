9e26b2de250fa643abe59f5137023cde9e3e2f80
= <=>
2021-10-13 19:46:25 +0300

    Admission price - homeWork

---
+++ AdmissionPriceHomework/AdmissionPriceHomework/main.swift
@@ -0,0 +1,60 @@
+//
+//  main.swift
+//  AdmissionPriceHomework
+//
+//  Created by Rayan Taj on 13/10/2021.
+//
+
+import Foundation
+
+
+
+// This variable is going to present the total cost for admission entering the zoo
+var totalCost = 0
+
+
+// while the user entering any number except 0 the loop will continue.
+while true {
+    
+    // Getting the input from the user
+    print("Enter a visitor age (0 to exit) :  " , terminator: "")
+    let UserInput = Utils.readInt()
+    
+        // when the user enters 0 , this event will trigger the condtion and exit the loop (using break)
+        if UserInput==0 {
+            
+            break
+        // if the user entered any other number...
+        }   else{
+        
+            
+            switch UserInput {
+            
+            // if the guest age is between 1 -3 (3 is NOT included)
+            // no charge will be added
+            case 1..<3 :
+            // if i remove the () it's gonna give an error because a switch case must have an executable code and in this case () will do nothing,
+            // just to prevent the error
+                ()
+                
+            // if the guest age is between 3 -13 (13 is NOT included)
+            // 14$ will be added
+            case 3..<13:
+                totalCost += 14
+                
+            // if the guest age is 65 or more
+            // 18$ will be added
+            case 65... :
+                totalCost += 18
+         
+            // anything else, 23$ will be added
+            default :
+                totalCost += 23
+         
+            }
+        }
+}
+
+//Print the total cost.
+print("The total cost: \(Double(totalCost))")
+
