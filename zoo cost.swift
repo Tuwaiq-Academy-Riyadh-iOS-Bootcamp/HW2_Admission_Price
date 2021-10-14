06ecbbcc54be99b28459ef40584b344a7ab06ba6
Areej Mohammad <areejmohammad@Areejs-MacBook-Air.local>
2021-10-14 09:56:09 +0300

    Areej Mashabi

---
+++ zoo cost.swift
@@ -0,0 +1,45 @@
+//
+//  zoo cost.swift
+//  swift_demo_T1000
+//
+//  Created by Areej Mohammad on 07/03/1443 AH.
+//
+
+import Foundation
+//
+//
+//
+//var ageguest: Int
+//
+//
+//
+//var cost = 0.0
+//
+//repeat {
+//    print("Enter a visitor age : ", terminator: "")
+//    ageguest = Utils.readInt()
+//    switch ageguest {
+//
+//    case ...2:
+//
+//        cost += 0.0
+//    case 3...12:
+//
+//        cost += 14.0
+//
+//    case 65...:
+//
+//        cost += 18.0
+//
+//    default :
+//        cost += 23.0
+//
+//    }
+//
+//}while ageguest != 0
+//
+////print (" the total cost: ", cost)
+//var c = String ( format: "%.2f", cost)
+//print ("The total cost is:", c ,"$")
+//
+//
